faf2baf7f7aaaac71bc673fc49b916ca1df990df
Pavel Koyushev <pkoyushev@gmail.com>
2023-11-13 11:53:02 +0300

    Initial Commit

---
+++ TCA-simple/TCA_simpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_simpleApp.swift
+//  TCA-simple
+//
+//  Created by Pavel Koyushev on 13.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct TCA_simpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
